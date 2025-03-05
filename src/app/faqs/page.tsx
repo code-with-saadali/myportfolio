4835013eddb3749292ca56be93474008d4bdc560
code-with-saadali <saadali786000786@gmail.com>
2025-03-05 12:23:35 +0500

    change name and add page

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -1,15 +1,12 @@
-// app/page.tsx
-'use client';
-import  {Header } from '../services/_components/Header';
-import { Hero } from '../faqs/_components/Hero';
-import { FaqSection } from '../faqs/_components/FaqSection';
-import { ContactSection } from '../services/_components/ContactSection';
+"use client";
+import { Hero } from "../faqs/_components/Hero";
+import { FaqSection } from "../faqs/_components/FaqSection";
+import { ContactSection } from "../services/_components/ContactSection";
 
 export default function FaqPage() {
   return (
     <div className="min-h-screen bg-black text-white">
-      <Header /> 
-      <Hero 
+      <Hero
         title="Frequently Asked Questions"
         subtitle="Find answers to common queries about our services"
       />
